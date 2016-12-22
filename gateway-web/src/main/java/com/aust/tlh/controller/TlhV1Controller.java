b0d588fb6cf52cf8629aa4cc4fbe94b0cc596d67
FrankTang <1713445978@qq.com>
2016-12-22 15:29:43 +0800

    常用util

--- gateway-web/src/main/java/com/aust/tlh/controller/TlhV1Controller.java
+++ gateway-web/src/main/java/com/aust/tlh/controller/TlhV1Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 /**
  * 类描述:
@@ -20,8 +21,38 @@ public class TlhV1Controller {
     @RequestMapping(value = "/test",method = RequestMethod.POST)
     @ResponseBody
     Object test(@RequestBody String req, HttpServletRequest httpServletRequest) {
+        String filePath = "/vj.key";
+        File file = new File(filePath);
+        try {
+            // read file content from file
+            StringBuffer sb= new StringBuffer("");
 
+            FileReader reader = new FileReader(file);
+            BufferedReader br = new BufferedReader(reader);
+
+            String str = null;
+
+            while((str = br.readLine()) != null) {
+                sb.append(str);
+            }
+            System.out.println(sb.toString().length());
+            br.close();
+            reader.close();
+
+            // write string to file
+        }
+        catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
         return "welcome";
+    }
+
+
+    public static void main(String[] args) {
+
     }
 
 }
